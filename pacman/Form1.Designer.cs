f1b5838c661103aa9170088020ed6fcabf52d106
s1121604-sudo <s1121604@syajh.tp.edu.tw>
2025-11-04 16:25:00 +0800

    Add files via upload

---
+++ pacman/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace PACMAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PACMAN = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PACMAN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Location = new System.Drawing.Point(190, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(58, 179);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Blue;
+            this.panel2.Location = new System.Drawing.Point(501, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(58, 179);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Blue;
+            this.panel3.Location = new System.Drawing.Point(276, 346);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(58, 179);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Blue;
+            this.panel4.Location = new System.Drawing.Point(584, 346);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(58, 179);
+            this.panel4.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox2.Location = new System.Drawing.Point(264, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // PACMAN
+            // 
+            this.PACMAN.Image = global::PACMAN.Properties.Resources.pacman;
+            this.PACMAN.Location = new System.Drawing.Point(43, 235);
+            this.PACMAN.Name = "PACMAN";
+            this.PACMAN.Size = new System.Drawing.Size(58, 58);
+            this.PACMAN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PACMAN.TabIndex = 4;
+            this.PACMAN.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox1.Location = new System.Drawing.Point(328, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox3.Location = new System.Drawing.Point(392, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox4.Location = new System.Drawing.Point(368, 369);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox5.Location = new System.Drawing.Point(471, 369);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox6.Location = new System.Drawing.Point(93, 50);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox7.Location = new System.Drawing.Point(584, 50);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 11;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(24, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(24, 393);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(24, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Y:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox8.Location = new System.Drawing.Point(648, 50);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 15;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox9.Location = new System.Drawing.Point(673, 369);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 16;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::PACMAN.Properties.Resources.dollar;
+            this.pictureBox10.Location = new System.Drawing.Point(28, 50);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(58, 54);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox10.TabIndex = 17;
+            this.pictureBox10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PACMAN);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Name = "Form1";
+            this.Text = "Pacman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PACMAN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox PACMAN;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
---
+++ pacman/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PACMAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool goup,godown,goright,goieft = false;
+        PictureBox[] pictureBoxes = new PictureBox[20];
+        Panel[] panels;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (goup)
+            {
+                PACMAN.Top -= 5;
+            }
+            if (godown)
+            {
+                PACMAN.Top += 5;
+            }
+            if (goright)
+            {
+                PACMAN.Left -= 5;
+            }
+            if (goieft)
+            {
+                PACMAN.Left += 5;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (PACMAN.Bounds.IntersectsWith(pictureBoxes[i].Bounds))
+                {
+                    pictureBoxes[i].Visible = false;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (PACMAN.Bounds.IntersectsWith(panels[i].Bounds))
+                {
+                    goup = godown = goieft = goright = false;
+                }
+            }
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox1.Bounds))
+            //{
+            //    pictureBox1.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox2.Bounds))
+            //{
+            //    pictureBox2.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox3.Bounds))
+            //{
+            //    pictureBox3.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox4.Bounds))
+            //{
+            //    pictureBox4.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox5.Bounds))
+            //{
+            //    pictureBox5.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox6.Bounds))
+            //{
+            //    pictureBox6.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox7.Bounds))
+            //{
+            //    pictureBox7.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox8.Bounds))
+            //{
+            //    pictureBox8.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox9.Bounds))
+            //{
+            //    pictureBox9.Visible = false;
+            //}
+            //if (PACMAN.Bounds.IntersectsWith(pictureBox10.Bounds))
+            //{
+            //    pictureBox10.Visible = false;
+            //}
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 5;
+            timer1.Enabled = true;
+
+            panels = new Panel[4]
+            {
+                panel1,panel2,panel3,panel4
+            };
+            
+            pictureBoxes = new PictureBox[10]
+            {
+                pictureBox1,pictureBox2,pictureBox3,pictureBox4,pictureBox5,pictureBox6,pictureBox7,pictureBox8,pictureBox9,pictureBox10
+            };
+        }
+        
+            
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Up)
+            //{
+            //    goup = false;
+            //}
+            //if (e.KeyCode == Keys.Down)
+            //{
+            //    godown = false;
+            //}
+            //if (e.KeyCode == Keys.Left)
+            //{
+            //    goright = false;
+            //}
+            //if (e.KeyCode == Keys.Right)
+            //{
+            //    goieft = false;
+            //}
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            goup = godown = goieft = goright = false;
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+                PACMAN.Image = Properties.Resources.pacmanup;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+                PACMAN.Image = Properties.Resources.pacmandown;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goright = true;
+                PACMAN.Image = Properties.Resources.pacmanleft;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goieft = true;
+                PACMAN.Image = Properties.Resources.pacman;
+            }
+            
+
+        }
+    }
+}
